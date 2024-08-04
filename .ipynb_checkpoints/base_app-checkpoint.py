e01e69a658365bda46548bcfbf9e5076b85f865b
Clement Mphethi <clementmphethi@gmail.com>
2024-08-04 13:04:11 +0200

    Update insights page

--- .ipynb_checkpoints/base_app-checkpoint.py
+++ .ipynb_checkpoints/base_app-checkpoint.py
@@ -86,13 +86,13 @@ elif page == "Insights":
                                    ["Top 10 Most Rated Animes", "Top 10 Least Rated Animes", "Top 10 Anime Genre Distribution", "Distribution of User Ratings"])
     
     if insights_option == "Top 10 Most Rated Animes":
-        st.image(load_image("images/top_10_most_rated_animes.jpg"), width=1000)
+        st.image(load_image("images/top_10_most_rated_animes.png"), width=800)
     elif insights_option == "Top 10 Least Rated Animes":
-        st.image(load_image("images/top_10_least_rated_animes.jpg"), width=1000)
+        st.image(load_image("images/top_10_least_rated_animes.png"), width=800)
     elif insights_option == "Top 10 Anime Genre Distribution":
-        st.image(load_image("images/top_10_anime_genre_distribution.jpg"), width=1000)
+        st.image(load_image("images/top_10_anime_genre_distribution.png"), width=800)
     elif insights_option == "Distribution of User Ratings":
-        st.image(load_image("images/distribution_of_user_ratings.jpg"), width=1000)
+        st.image(load_image("images/distribution_of_user_ratings.png"), width=800)
 
 # Anime Archive page
 elif page == "Anime Archive":
